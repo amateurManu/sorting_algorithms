ae5afb531239a588654ede79a8028a258c7950a1
amateurManu <oderoemmanuel6@gmail.com>
2024-02-16 02:36:59 -0800

    made changes

--- sort.h
+++ sort.h
@@ -22,7 +22,7 @@ typedef struct listint_s
 
 /* mandatory tasks */
 void print_array(const int *array, size_t size);
-void bubble_sort(int *array, size_t size):
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
@@ -36,6 +36,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
+/* void sort_deck(deck_node_t **deck); */
 
 #endif /* SORT_H */
